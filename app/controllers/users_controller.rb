e8dd9e228c9c6b37b89c0b4b84f1067bdac592d9
tsyan <anna.tsykalova@gmail.com>
2014-01-30 06:57:16 -0500

    updated user index/new/create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def new # the form
+		@user = User.new
+	end
+
+	def create # update database
+		@user = User.new(user_params)
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email)
+	end
+
 end
